8722882e1b1dc3028072b7ee24893f8908e99983
Farhan Riski <farhanriski40@gmail.com>
2023-07-12 18:08:19 +0700

    perbaharuan

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        costum: {
+          50 : '#862B0D' ,
+
+        }
+      }
+    },
+    screens: {
+      'tablet': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'laptop': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'desktop': '1280px',
+      // => @media (min-width: 1280px) { ... }
+    },
   },
   plugins: [],
 }
